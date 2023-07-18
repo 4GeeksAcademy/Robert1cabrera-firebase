7636f03bdf9ed022008bf76f03da85a56667f146
Robert <Robert1cabrera@gmail.com>
2023-07-18 01:11:11 +0000

    progress

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,26 @@ const axios = require("axios");
 const cors = require("cors")({ origin: true });
 require("dotenv").config();
 
+
+
+function formatHourTime(hour) {
+
+  const unformattedTime = hour.time;
+
+  const numericTime = Number(hour.time.slice(11, 13));
+  let formattedTime = "";
+
+  if (numericTime >= 0 && numericTime <= 11) {
+    formattedTime = `${numericTime} AM`;
+  }
+  else {
+    const pmTime = numericTime - 12 === 0 ? 12 : numericTime - 12;
+    formattedTime = `${pmTime} PM`;
+  }
+
+  return formattedTime;
+}
+
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
@@ -23,6 +43,14 @@ exports.getDayWeather = onRequest(async (req, res) => {
       const { data } = await axios(
         `https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=Miami&days=1&aqi=no&alerts=no`
       );
+
+      const hours = data.forecast.forecastday[0].hour;
+
+      for (let i = 0; i < hours.length; i ++) {
+        const hour = hours[i];
+
+        hour.formatted_time = formatHourTime(hour);
+      }
 
       response.data = data;
     } catch (e) {
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,48 +3,65 @@ import "./App.css";
 
 const backendHostUrl = 'https://robert1cabrera-obscure-space-journey-jrr9gq7jvwqh57g9-5001.preview.app.github.dev'
 
-
+const date = new Date(); 
 function App() {
   const [location, setLocation] = useState(0);
   const [currentTemp, setCurrentTemp] = useState(0);
   const [lowTemp, setLowTemp] = useState(0);
   const [highTemp, setHighTemp] = useState(0);
-  const [icon, setIcon] = useState(0);
-  const [condition, setCondition] = useState(0);
-  const [hour, setHour] = useState(0);
+  const [icon, setIcon] = useState('');
+  const [condition, setCondition] = useState('');
+  const [hours, setHours] = useState([]);
 
   const [dataLoaded, setDataLoaded] = useState(false);
   useEffect(() => {
     (async () => {
-      const res = await fetch(
-        `${backendHostUrl}/geeks-firebase-72e6d/us-central1/getDayWeather`
-      );
 
-      const data = await res.json();
-      setLocation(data.data.location.name)
-      setCurrentTemp(data.data.current.temp_f);
-      setLowTemp(data.data.forecast.forecastday[0].day.mintemp_f);
-      setHighTemp(data.data.forecast.forecastday[0].day.maxtemp_f);
-      setIcon(data.data.current.condition.icon);
-      setCondition(data.data.forecast.forecastday[0].day.condition.text);
-      setHour(data.data.forecast.forecastday[0].hour[0]);
-      setDataLoaded(true)
+      try {
 
-      console.log("The res: ", data.data);
+        const res = await fetch(
+          `${backendHostUrl}/geeks-firebase-72e6d/us-central1/getDayWeather`
+        );
+
+        const data = await res.json();
+
+        console.log(data);
+        setLocation(data.data.location.name)
+        setCurrentTemp(data.data.current.temp_f);
+        setLowTemp(data.data.forecast.forecastday[0].day.mintemp_f);
+        setHighTemp(data.data.forecast.forecastday[0].day.maxtemp_f);
+        setIcon(data.data.current.condition.icon);
+        setCondition(data.data.forecast.forecastday[0].day.condition.text);
+        setHours(data.data.forecast.forecastday[0].hour);
+        setDataLoaded(true)
+
+        console.log("The res: ", data.data);
+      }
+      catch (e) {
+        console.error(e);
+      }
     })();
   }, []);
   if (!dataLoaded) return <p> ... Loading</p>
   return (
     <div className="App">
       <h1 className="city"> {location} </h1>
-      <h2 className="temp">{currentTemp}</h2>
+      <h2 className="temp">{currentTemp}°</h2>
       <h3 className="condit">{condition}</h3>
-      <h3 className="low-high">L:{lowTemp}  H:{highTemp}</h3>
+      <h3 className="low-high">L:{lowTemp} ˚  H:{highTemp} ˚</h3>
       <div className="hour-cast">
-
-        {icon}
-        {hour}
-
+        <h5>🕑HOURLY FORECAST</h5>
+        <img src={icon} alt="weather icon" width="50" height="60" />
+        <div className="hours">
+          {hours.map((hour) => (
+            <div>
+              {!!hour.now && "now"}
+              {hour.formatted_time}
+             
+              
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
